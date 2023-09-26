a90ef8a2240f1dcfe70f936d16965fb3d55131d4
Itzel016 <clopeza016@alumno.uaemex.mx>
2023-09-26 08:57:57 -0600

    DD

---
+++ LabelFrame.java
@@ -0,0 +1,8 @@
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class   
